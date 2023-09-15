c1a4afc915539113f4bd618dc1461e2b9210b2ad
fwpv <fwpv@protonmail.com>
2023-09-15 12:32:26 +0500

    Corrected

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <iostream>
 #include <map>
+#include <numeric>
 #include <set>
 #include <string>
 #include <utility>
@@ -89,7 +90,8 @@ public:
 
         sort(matched_documents.begin(), matched_documents.end(),
              [](const Document& lhs, const Document& rhs) {
-                 if (abs(lhs.relevance - rhs.relevance) < 1e-6) {
+                 const double EPSILON = 1e-6;
+                 if (abs(lhs.relevance - rhs.relevance) < EPSILON) {
                      return lhs.rating > rhs.rating;
                  } else {
                      return lhs.relevance > rhs.relevance;
@@ -156,7 +158,7 @@ private:
         if (ratings.empty()) {
             return 0;
         }
-        int rating_sum = 0;
+        int rating_sum = accumulate(ratings.begin(), ratings.end(), 0);
         for (const int rating : ratings) {
             rating_sum += rating;
         }
